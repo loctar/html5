694f6a0632b737d96cd59980d7fabd2c69683276
loctar.zhao <shixuanzhao@yahoo.cn>
2013-10-21 21:30:39 +0800

    update 1st

---
+++ src/test/Test.java
@@ -0,0 +1,26 @@
+package test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: loctar
+ * Date: 13-7-15
+ * Time: 下午10:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print("Hello World");
+    }
+}
